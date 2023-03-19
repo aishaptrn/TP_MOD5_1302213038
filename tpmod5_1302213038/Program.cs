6ddb7a6267af14ae9884a623c723225d05a5bd35
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-03-19 17:57:00 +0700

    menambahkan class DataGeneric

--- tpmod5_1302213038/Program.cs
+++ tpmod5_1302213038/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class DataGeneric<T>
+{
+    private T data;
+    
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void printData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class MainProgram
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("1302213038");
+        data.printData();
+    }
+}
